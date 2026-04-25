594da093fb950d09ec29b616387694cea79e5d8a
ays19 <sahsanyasir@gmail.com>
2026-04-25 20:15:56 +0600

    add Tag and TaggedItem models for generic relations

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    #What tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type (product, video, article)
+    # ID
+    product = models.ForeignKey(ContentType)
