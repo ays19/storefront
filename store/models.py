b5d592a5333c220729730465c195c7848a4b3f4f
ays19 <sahsanyasir@gmail.com>
2026-04-25 16:45:31 +0600

    create 2 classes for product ans customer with attributes

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    First_name = models.CharField(max_length=255)
+    Last_name = models.CharField(max_length=255)
+    E-mail = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    
+
