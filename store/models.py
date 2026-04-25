a30d8d4917b4d225ac28f9cfedac5393e07ebb69
ays19 <sahsanyasir@gmail.com>
2026-04-25 18:04:46 +0600

    add membership level choices for Customer model

--- store/models.py
+++ store/models.py
@@ -8,10 +8,21 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
     First_name = models.CharField(max_length=255)
     Last_name = models.CharField(max_length=255)
     E-mail = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
 
