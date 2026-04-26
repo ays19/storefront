d900877f4ad057d0ecc8341f267cbb1963bb89b4
ays19 <sahsanyasir@gmail.com>
2026-04-26 07:23:36 +0600

    add custom table name and indexes for Customer model

--- store/models.py
+++ store/models.py
@@ -29,12 +29,19 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
 
-    First_name = models.CharField(max_length=255)
-    Last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
+
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
