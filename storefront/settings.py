b4d09b4f5bbfb4b85b3732c10e88e399e60022dc
ays19 <sahsanyasir@gmail.com>
2026-04-27 18:03:59 +0600

    feat: configure MySQL backend and apply migrations

--- storefront/settings.py
+++ storefront/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': 'Ays19@Ubuntu26',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
