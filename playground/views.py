39227f7e1dce44e6311b28ee278a09b5789b7b03
ays19 <sahsanyasir@gmail.com>
2026-04-28 10:22:45 +0600

    implement and document Product queryset field lookups

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
+    
+    #product = Product.objects.filter(pk=0).first() #can use try-catch but it is better
+    #exists = Product.objects.filter(pk=0).exists()
+    #queryset = Product.objects.filter(unit_price__range=(20, 30)) #can not use logical operations here, cause it is bolean expresion. so use keyword arguments like gt=greatar than ,gte= greater equal etc. for more search queryset api then fiels lookup
+    #queryset = Product.objects.filter(collection__id__range=(1, 2, 3))
+    #queryset = Product.objects.filter(title__icontains='coffee') #i for remove case sensitivity
+    #queryset = Product.objects.filter(last_update__year=2021)
+    queryset = Product.objects.filter(description__isnull=True)
 
-    for product in query_set:
-        print(product)
-    return render(request, 'hello.html', {'name': 'Sharar'})
+    return render(request, 'hello.html', {'name': 'Sharar', 'products': list(queryset)}) 
