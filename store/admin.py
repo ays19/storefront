3f989a7a275c24ec8a5dac8203c99c8e69561dc0
ays19 <sahsanyasir@gmail.com>
2026-05-05 11:13:47 +0600

    Add orders count with clickable link to CustomerAdmin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 from django.db.models import Count
+from django.utils.html import format_html
 
 # Register your models here.
 
@@ -24,10 +25,26 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10    
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist') 
+            + '?' 
+            + urlencode({
+                'customer_id': str(customer.id)
+                }))
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -39,7 +56,14 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist') 
+            + '?' 
+            + urlencode({
+                'collection_id': str(collection.id)
+                }))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+         
     
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(products_count=Count('product'))
