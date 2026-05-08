33edcbe0f918132896d24a899702139ead6f835f
ays19 <sahsanyasir@gmail.com>
2026-05-08 15:31:52 +0600

    feat: add description, slug, and inventory fields to ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from itertools import product
 from rest_framework import serializers
 from store.models import Product, Collection
 
@@ -10,7 +11,7 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
 
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     # id = serializers.IntegerField()
@@ -27,4 +28,4 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal('1.1')
-     
+    
