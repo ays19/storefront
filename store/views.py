f0178a1a1a1f797837b951d4780bc2dac95d9d20
ays19 <sahsanyasir@gmail.com>
2026-05-08 06:57:32 +0600

    add custom fields and relationships to ProductSerializer

--- store/views.py
+++ store/views.py
@@ -9,11 +9,12 @@ from .serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def product_list(request):
-    return Response("OK")
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id): 
     product = get_object_or_404(Product, pk=id)
-    product = Product.objects.get(pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
