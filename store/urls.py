3387bdb62578bc16482d96095f90d8c4a3a4e182
ays19 <sahsanyasir@gmail.com>
2026-05-09 08:42:53 +0600

    ix ProductDetail methods and add delete logic

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 #URL Configuration
 urlpatterns = [
     path('products/', views.ProductList.as_view()), #as_view method convert the class regular function based view 
-    path('products/<int:id>/', views.product_detail),
+    path('products/<int:id>/', views.ProductDetail.as_view()),
     path('collections/', views.collection_list),
     path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
 ]   
--- store/views.py
+++ store/views.py
@@ -30,13 +30,6 @@ class ProductDetail(APIView):
         product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    
-    def post(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
 
     def put(self, request, id):
         product = get_object_or_404(Product, pk=id)
@@ -44,6 +37,13 @@ class ProductDetail(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def collection_list(request):
