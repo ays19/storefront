3971e3d87771688d1f232618c72a523c7242dd79
ays19 <sahsanyasir@gmail.com>
2026-05-10 11:12:10 +0600

    implement nested routing for product reviews
    Integrated drf-nested-routers to create a nested resource for reviews under products.
    
    Updated ReviewViewSet to filter querysets based on product_pk from URL kwargs.
    
    Modified ReviewSerializer to handle manual review creation using product_id from the context.
    
    Configured NestedDefaultRouter in urls.py.

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter, DefaultRouter
+#from rest_framework.routers import SimpleRouter, DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
 #router = SimpleRouter()  #create router onject
-router = DefaultRouter()  #create router object
+#router = DefaultRouter()  #create router object
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet) #register the viewset with the router
 router.register('collections', views.CollectionViewSet)  #register the viewset with the router
 
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 #URL Configuration
-urlpatterns = router.urls
+urlpatterns = router.urls + products_router.urls
