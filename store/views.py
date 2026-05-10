4a758c78796467544eee849d68bc22244d940ab2
ays19 <sahsanyasir@gmail.com>
2026-05-10 16:04:46 +0600

    implement custom pagination for products
    Created pagination.py with DefaultPagination class setting a page size of 10.
    
    Integrated DefaultPagination into ProductViewSet.
    
    Cleaned up ViewSet configuration by removing inline pagination logic.

--- store/views.py
+++ store/views.py
@@ -1,9 +1,8 @@
-from store.filters import ProductFilter
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.pagination import PageNumberPagination
+#from rest_framework.pagination import PageNumberPagination
 #from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 #from rest_framework.decorators import api_view
@@ -11,6 +10,8 @@ from rest_framework.response import Response
 #from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from store.pagination import DefaultPagination
+from .filters import ProductFilter
 from .models import OrderItem, Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
@@ -20,6 +21,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
