5dfb05a79de6c73a45ec2f32f203fccdcf65bd60
ays19 <sahsanyasir@gmail.com>
2026-05-11 16:27:28 +0600

    migrate to email-based authentication and refresh migrations

--- storefront/settings.py
+++ storefront/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',   
     'store',
-    'store_custom',
     'tags',
     'likes',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+AUTH_USER_MODEL = 'core.User'
