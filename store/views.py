63379b504775acab04dd2d1eb2abeb7fc9525ad8
ays19 <sahsanyasir@gmail.com>
2026-05-11 13:06:50 +0600

    implement cart item update and delete functionality
    Created UpdateCartItemSerializer to handle quantity-only updates for existing cart items.
    
    Overrode get_serializer_class in CartItemViewSet to use specific serializers for POST, PATCH, and GET methods.
    
    Optimized CartItemViewSet performance by adding select_related('product') to the queryset.
    Configured the API to support partial updates (PATCH) and deletions for cart items.

--- store/views.py
+++ store/views.py
@@ -14,7 +14,7 @@ from rest_framework import status
 from store.pagination import DefaultPagination
 from .filters import ProductFilter
 from .models import Cart, CartItem, OrderItem, Product, Collection, Review
-from .serializers import CartItemSerializer, CartSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer
+from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer, UpdateCartItemSerializer
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
@@ -64,7 +64,17 @@ class CartViewSet(CreateModelMixin,
     serializer_class = CartSerializer
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
