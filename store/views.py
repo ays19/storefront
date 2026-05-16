4be9d89b2af92616790e5e44dc697bb41488d126
ays19 <sahsanyasir@gmail.com>
2026-05-16 09:58:30 +0600

    Refactor customer endpoint to handle profile retrieval and updates securely
    - Set `user_id` to `read_only=True` on `CustomerSerializer` to prevent users from altering their linked account ID.
    - Update `@action(detail=False)` on the `me` method to support both 'GET' and 'PUT' HTTP methods.
    - Replace `Customer.objects.get` with `get_or_create` to gracefully handle new users fetching or setting up their profile.
    - Implement data validation and saving logic for profile updates via 'PUT' requests.

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import AllowAny, IsAuthenticated
 #from rest_framework.pagination import PageNumberPagination
 #from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
@@ -83,9 +84,21 @@ class CartItemViewSet(ModelViewSet):
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
-    @action(detail=False)
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request):
-        customer = Customer.objects.get(user_id=request.user.id)
-        serializer = CustomerSerializer(customer)
-        return Response(serializer.data)
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
