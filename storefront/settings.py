0f6871d087b06cf21f7577ab52a60fdcbbdfc792
ays19 <sahsanyasir@gmail.com>
2026-05-19 07:27:43 +0600

    feat: configure CORS for local development frontend
    Added `django-cors-headers` to installed apps and middleware,
    and defined `CORS_ALLOWED_ORIGINS` for port 8001.

--- storefront/settings.py
+++ storefront/settings.py
@@ -34,6 +34,11 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
+]
+
 
 # Application definition
 
@@ -45,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'playground',
@@ -56,6 +62,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
