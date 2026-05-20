8aa7bf1fc4149b49f864834705cf5d82a17bfb1b
ays19 <sahsanyasir@gmail.com>
2026-05-20 20:35:36 +0600

    test: configure pytest.ini and add django_db decorator to test class

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,7 +1,11 @@
 from rest_framework import status
 from rest_framework.test import APIClient
+import pytest
 
+    
+@pytest.mark.django_db
 class TestCreateCollection:
+    #@pytest.mark.skip
     def test_if_user_is_anonymous_returns_401(self):
         #tests should have 3parts AAA
         #Arrange(this test has no arrange)
