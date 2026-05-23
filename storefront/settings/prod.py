128649893529914f3853acf7e67b8aea27a75848
ays19 <sahsanyasir@gmail.com>
2026-05-23 17:56:33 +0600

    Configure project for Railway deployment

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -6,4 +6,16 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sharar-prod.railway.app']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['MYSQLDB'],
+        'USER': os.environ['MYSQLUSER'],
+        'PASSWORD': os.environ['MYSQLPASSWORD'],
+        'HOST': os.environ['MYSQLHOST'],
+        'PORT': os.environ['MYSQLPORT'],
+        'CONN_MAX_AGE': 600,
+    }
+}
